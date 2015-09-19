68d90aa847ef6016dc3caf919d4eb0239efb66bb
Gustavo Magalhaes <itsgustavopereira@gmail.com>
2015-09-19 10:17:17 -0700

    :)

--- rsi/demos/captura-web.py
+++ rsi/demos/captura-web.py
@@ -8,8 +8,8 @@ for ts, pkt in pcap.pcap("web.pcap"):
 	ip = eth.data
 	tcp = eth.data.data
 
-	print(str(nPkts)+" "+socket.inet_ntoa(ip.src)+" "+socket.inet_ntoa(ip.dst)+" "+repr(tcp.sport)+" "+repr(tcp.dport)+" "+repr(ip.len)+" "+repr(tcp.flags))
-	print("\n")
+	print(str(nPkts)+"\t"+socket.inet_ntoa(ip.src)+"\t"+socket.inet_ntoa(ip.dst)+"\t"+repr(tcp.sport)+"\t"+repr(tcp.dport)+"\t"+repr(ip.len)+"\t"+repr(tcp.flags))
+	#print("\n")
 	
 	#print("Pacote puro #"+str(nPkts))
 	#print(dpkt.hexdump(pkt))
