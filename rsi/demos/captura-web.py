58490e6f4e24d832b9bee837769fe6ef3398a1e8
Gustavo Magalhaes <itsgustavopereira@gmail.com>
2015-09-19 08:06:20 -0700

    Testing

--- rsi/demos/captura-web.py
+++ rsi/demos/captura-web.py
@@ -6,15 +6,16 @@ for ts, pkt in pcap.pcap("web.pcap"):
 	nPkts += 1
 	eth = dpkt.ethernet.Ethernet(pkt) #extraindo dados do pacote
 	
+	
 	print(str(nPkts)+" "+repr(eth.data.data.dport)+" "+repr(eth.data.data.sport))
 
 	#print("Pacote puro #"+str(nPkts))
 	#print(dpkt.hexdump(pkt))
 
 	#print("Mostrando o pacote #"+str(nPkts))
-	
-	#print(ts, repr(eth))
-	
+	print("\n")
+	print(ts, repr(eth))
+	print("\n")
 	#print("Mostrando o endereco de destino do pacote #"+str(nPkts))
 	#print(repr(eth.dst))
 	#print("\n")
