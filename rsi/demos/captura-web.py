571ef385d368feaae4def6b10e5f739b4ac26b53
Gustavo Magalhaes <itsgustavopereira@gmail.com>
2015-09-19 16:40:39 +0000

    New code

--- rsi/demos/captura-web.py
+++ rsi/demos/captura-web.py
@@ -7,24 +7,13 @@ for ts, pkt in pcap.pcap("web.pcap"):
 	eth = dpkt.ethernet.Ethernet(pkt) #extraindo dados do pacote
 	ip = eth.data
 	tcp = eth.data.data
-	
-	print("IP src e IP Dst")
-	print(socket.inet_ntoa(ip.src)+" "+socket.inet_ntoa(ip.dst))
+
+	print(str(nPkts)+" "+socket.inet_ntoa(ip.src)+" "+socket.inet_ntoa(ip.dst)+" "+repr(tcp.sport)+" "+repr(tcp.dport)+" "+repr(ip.len)+" "+repr(tcp.flags))
 	print("\n")
-
-	print("No., TCP origem, TCP destino , tamanho IP, tamanho TCP")
-	print(str(nPkts)+" "+repr(tcp.sport)+" "+repr(tcp.dport)+" "+repr(ip.len)+" "+repr(tcp.flags))
-
+	
 	#print("Pacote puro #"+str(nPkts))
 	#print(dpkt.hexdump(pkt))
 
-	#print("Mostrando o pacote #"+str(nPkts))
-	print("\n")
-	print(ts, repr(eth))
-	print("\n")
-	#print("Mostrando o endereco de destino do pacote #"+str(nPkts))
-	#print(repr(eth.dst))
-	#print("\n")
 	
 	
 
