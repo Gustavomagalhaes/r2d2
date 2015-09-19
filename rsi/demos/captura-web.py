88495ae08c40e63b611fa14faabc3526f3d879a1
Gustavo Magalhaes <itsgustavopereira@gmail.com>
2015-09-19 09:03:20 -0700

    aff cade o IP?

--- rsi/demos/captura-web.py
+++ rsi/demos/captura-web.py
@@ -5,9 +5,16 @@ nPkts=0
 for ts, pkt in pcap.pcap("web.pcap"):
 	nPkts += 1
 	eth = dpkt.ethernet.Ethernet(pkt) #extraindo dados do pacote
+	ip = eth.data
+	tcp = eth.data.data
 	
-	print("No., TCP origem, TCP destino , tamanho HTTP")
-	print(str(nPkts)+" "+repr(eth.data.data.sport)+" "+repr(eth.data.data.dport)+" "+repr(eth.data.len))
+	print("IP source e IP Dest")
+	print(repr(eth.src)+" "+repr(eth.dst))
+
+	print("\n")
+
+	print("No., TCP origem, TCP destino , tamanho IP, tamanho TCP")
+	print(str(nPkts)+" "+repr(tcp.sport)+" "+repr(tcp.dport)+" "+repr(ip.len)+" "+repr(tcp.flags))
 
 	#print("Pacote puro #"+str(nPkts))
 	#print(dpkt.hexdump(pkt))
