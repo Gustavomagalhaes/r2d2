acb2c32b8425098d28574a6339599acca0b5d779
Gustavo Magalhaes <itsgustavopereira@gmail.com>
2015-09-19 20:17:39 +0000

    Len

--- rsi/demos/captura-web.py
+++ rsi/demos/captura-web.py
@@ -7,7 +7,14 @@ for ts, pkt in pcap.pcap("web.pcap"):
 	eth = dpkt.ethernet.Ethernet(pkt) #extraindo dados do pacote
 	ip = eth.data
 	tcp = eth.data.data
-	print(str(nPkts)+"\t"+socket.inet_ntoa(ip.src)+"\t"+socket.inet_ntoa(ip.dst)+"\t"+repr(tcp.sport)+"\t"+repr(tcp.dport)+"\t"+repr(ip.len)+"\t"+repr(tcp.flags))
+	#print(str(nPkts)+"\t"+socket.inet_ntoa(ip.src)+"\t"+socket.inet_ntoa(ip.dst)+"\t"+repr(tcp.sport)+"\t"+repr(tcp.dport)+"\t"+repr(ip.len)+"\t"+repr(tcp.flags))
+	print(str(nPkts)+"\t"+socket.inet_ntoa(ip.src)+"\t"+socket.inet_ntoa(ip.dst)+"\t"+repr(tcp.sport)+"\t"+repr(tcp.dport)+"\t"+pcap.len)
+	
+	if (nPkts == 198) :
+		print(pcap.len)
+	
 	
 	#print("Pacote puro #"+str(nPkts))
 	#print(dpkt.hexdump(pkt))
+	
+	
