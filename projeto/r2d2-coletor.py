7d8bd106a7e87132263edf56df2fe147ee6361a5
Robson Ugo Souza <ulrichugo@gmail.com>
2015-10-12 06:42:14 +0000

    separação do consumer, recriação do monitor e do coletor

--- projeto/r2d2-coletor.py
+++ projeto/r2d2-coletor.py
@@ -1,100 +1,180 @@
 # -*- coding: cp1252 -*-
 #PARA PARAR O PROCESSO PARALELO DO COLETOR USAR sudo pkill -f r2d2-coletor.py
 import os
-import socket, traceback
+import socket, traceback, time, pcap, dpkt, re, pika, sys
 from thread import *
 
 class Coletor:
     
-    def __init__(self):
+    def __init__(self, ip):
         self.tamanhoPacote = 1024
         self.hostBroadcast = ''
         self.portaEnvioBroadcast = 9000
         self.portaRecebeBroadcast = 9001
+        self.ip = ip
         self.envioBroadcast = '<broadcast>'
         self.broadcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.status="0"
+        self.broadcastSocket.settimeout(10)
+        self.status = True
         
     def abrirConexao(self):
         self.broadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.broadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        self.broadcastSocket.settimeout(10)
-        self.broadcastSocket.bind((self.hostBroadcast, self.portaRecebeBroadcast))
         
         while True:
-            self.broadcastSocket.sendto("DESCOBRIR", (self.envioBroadcast, self.portaEnvioBroadcast))
-            print("CP3O: Descobrindo monitor...")
+            self.broadcastSocket.sendto("MONITOR", (self.envioBroadcast, self.portaRecebeBroadcast))
+            print("Descobrindo...")
             
             try:
-                mensagem, endereco = self.broadcastSocket.recvfrom(2048)
-                if str(mensagem) == "DESCOBERTO":
-                    print "CP3O: Descoberto monitor " + str(endereco)
-                    self.broadcastSocket.settimeout(None)
-                    mensagemMonitor = 'C3PO: Aguardando comando...' #primeira msg recebida pelo coletor quando o comando for passado
-                    self.broadcastSocket.sendto(mensagemMonitor, endereco)
-                    return False
+                message, serverAddress = self.broadcastSocket.recvfrom(2048)
+                if str(message) == "APRESENTAR":
+                    print("Monitor " + serverAddress[0] + " descoberto.")
+                    print("Aguardando comando.")
+                    self.broadcastSocket.close()
+                    break
             except Exception:
                 continue
-        
-    def getBroadcastSocket(self):
-        return self.broadcastSocket
-        
-    def getTamanhoPacote(self):
-        return self.tamanhoPacote
-        
-    def getPortaEnvioBroadcast(self):
-        return self.portaEnvioBroadcast   
-        
-    def getStatus(self):
-        return self.status
-        
-    def setStatus(self, status):
-        self.status = status
+            
     
-    def trocaStatus(self, status):
-        if status=="0":
-            self.setStatus("1")
-        elif status=="1":
-            self.setStatus("0")
-        return self.getStatus()
+    def receberComandoUnicast(self):
+        try:
+            self.broadcastSocket.bind(("", 9000))
+        except:
+            self.broadcastSocket.close()
+            self.broadcastSocket.bind(("", 9000))
+            
+        
+        while True:
+            try:
+                
+                message, clientAddress = self.broadcastSocket.recvfrom(2048)
+                if message == "PARAR":
+                    self.status = False
+                elif message == "CONTINUAR":
+                    self.status = True
+                    
+                print("Ok")
+                self.broadcastSocket.sendto("OK", clientAddress)
+            except (KeyboardInterrupt, SystemExit):
+                self.broadcastSocket.close()
+                
+            except:
+                continue
+            
+    def emitMsg(self, routing_key, msg):
+        credentials = pika.PlainCredentials('skywalker', 'luke')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+               self.getIp(), 5672, '/starwars', credentials))
+        channel = connection.channel()
+        channel.exchange_declare(exchange='topic_logs',type='topic')
+        channel.basic_publish(exchange='topic_logs',routing_key=routing_key,body=mensagemProtocolo)
+        
+        print " [x] Sent %r:%r" % (routing_key, mensagemProtocolo)
+        
+        connection.close()
+        
+    def getIp(self):
+        return self.ip
+    
+    def setIp(self, ip):
+        self.ip = ip
+        
+    
+    def listarProtocolos(self):
+            # diretorio = os.listdir(os.getcwd()+'//l7-pat')
+            # protocolos = {}
+    
+            # for arquivo in diretorio:
+            #     arquivo = './/l7-pat//'+ arquivo 
+    
+            #     file = open(arquivo, 'r')
+            #     linha = file.readline()
+                
+            #     while linha != "":        
+            #         if linha[0] != "#":
+            #             if linha[0] != "^" and linha[:5] != "http/" and linha[0] != "\n":
+            #                 chave = str(linha.replace("\n",""))
+            #             elif  (linha[0] == "^" or linha[:5] == "http/") and linha[0] != "\n":
+            #                 valor = (str(linha).lstrip()).rstrip()
+            #         linha = file.readline()
+                    
+            #     protocolos[chave] = re.compile(valor)
+            protocolos = {"ssl":"^(.?.?\x16\x03.*\x16\x03|.?.?\x01\x03\x01?.*\x0b)", "ssh":"^ssh-[12]\.[0-9]", "ssdp":"^notify[\x09-\x0d ]\*[\x09-\x0d ]http/1\.1[\x09-\x0d -~]*ssdp:(alive|byebye)|^m-search[\x09-\x0d ]\*[\x09-\x0d ]http/1\.1[\x09-\x0d -~]*ssdp:discover", "bittorrent":"^(\x13bittorrent protocol|azver\x01$|get /scrape\?info_hash=)", "dhcp":"^[\x01\x02][\x01- ]\x06.*c\x82sc","http":"[\x09-\x0d -~]*"}
+                
+            return protocolos
+            
+    def iniciarColetor(self, file="", tempo = 60):
+        cont = 0
+        timeIni = time.time()
+        protocolos = self.listarProtocolos()
+        
+        for ts, pkt in pcap.pcap(file):
+            if self.status == True:
+                time.sleep(1)
+                
+                try:
+                    cont += 1
+                    timeAtual = time.time()
+                    if timeAtual - timeIni > tempo:
+                        break
+                    
+                    enl = dpkt.ethernet.Ethernet(pkt)
+                    layerRede = "NA"
+                    layerTrans = "NA"
+                    layerApp = "NA"
+                    print("Pacote " + cont + " da camada de enlace - Ethernet")
+                    rede = enl.data
+                    
+                    #rede
+                    if type(rede) != (""):
+                        if (rede == dpkt.ip.IP):
+                            layerRede = "IP"
+                        elif (rede == dpkt.arp.ARP):
+                            layerRede = "ARP"
+                        else:
+                            layerRede = "UNKNOWN"
+                        msgRede = ("Rede: " + layerRede + " | Timestamp: " +str(ts)+ " Tamanho: " + str(len(pkt)))
+                        #emitir(layerRede, msgRede)
+                        trns = rede.data
+                        
+                        #transporte
+                        if type(trns) != (""):
+                            if (trns == dpkt.tcp.TCP):
+                                layerTrans = "TCP"
+                            elif (trns == dpkt.udp.UDP):
+                                layerTrans = "UDP"
+                            else:
+                                layerTrans = "UNKNOWN"
+                            msgTrans = ("Transporte: " + layerTrans+ " | Rede: " + layerRede + " | Timestamp: " +str(ts)+ " Tamanho: " + str(len(pkt)))
+                            #emitir(layerTrans, msgTrans)
+                            app = trns.data.lower()
+                            found = False
+                            
+                            #aplicação
+                            for p in protocolos.items():
+                                expressao = re.compile(p[1])
+                                if expressao.search(app):
+                                    msgTrans = ("Aplicação: " + p[0] + " | Transporte: " + layerTrans+ " | Rede: " + layerRede + " | Timestamp: " +str(ts)+ " Tamanho: " + str(len(pkt)))
+                                    #emitir(layerApp, msgApp)
+                                    found = True
+                					
+                                if (not found):
+                                    msgTrans = ("Aplicação: " + str(app) + " | Transporte: " + layerTrans+ " | Rede: " + layerRede + " | Timestamp: " +str(ts)+ " Tamanho: " + str(len(pkt)))
+                    
+                    msg = "Aplicação: " + str(app) + " | Transporte: " + layerTrans+ " | Rede: " + layerRede + " | Timestamp: " +str(ts)+ " Tamanho: " + str(len(pkt))
+                    print msg
+                    #emititr
+                
+                except:
+                    print""
+            else:
+                time.sleep(1)
 
 if __name__ == '__main__':
     
     print("C3PO: Coletor iniciado")
     
-    c3po = Coletor()
+    c3po = Coletor("ip")
     c3po.abrirConexao()
-    broadcastSocket = c3po.getBroadcastSocket()
-    
-    
-    #thread = Thread()
-    
-    while True :
-        try:
-            mensagemComando, endereco = broadcastSocket.recvfrom(c3po.getTamanhoPacote())
-            
-            if mensagemComando == "CAPTURAR": # and thread.getStatus() == None:
-                broadcastSocket.sendto("C3PO: Capturando...", endereco)
-                print("C3PO: Capturando...")
-                #thread.start()
-                
-            elif mensagemComando == "CAPTURAR": # and thread.getStatus() == False:
-                broadcastSocket.sendto("C3PO: Capturando...", endereco)
-                
-            elif mensagemComando == "CAPTURAR": # and thread.getStatus() == True:
-                broadcastSocket.sendto("C3PO: Capturando...", endereco)
-                #thread.setStatus(False)
-            
-            elif mensagemComando == "SUSPENDER":
-                broadcastSocket.sendto("C3PO: Suspenso", endereco)
-                print("C3PO: Supenso")
-                #thread.setStatus(True)
-            
-            elif mensagemComando == "CONTINUAR":
-                broadcastSocket.sendto("C3PO: Capturando...", endereco)
-                #thread.setStatus(False)
-                
-                
-        except (KeyboardInterrupt, SystemExit):
-             os.system('clear')
-             break
+    c3po.receberComandoUnicast()
+    c3po.iniciarColetor("test.pcap",10000)
--- projeto/r2d2-monitor.py
+++ projeto/r2d2-monitor.py
@@ -13,49 +13,51 @@ class Monitor:
         self.broadcastSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         
         self.coletoresConectados={}
-        self.coletorAtual=""
-        
-        self.iniciarMonitor()
-        
-        self.channelRabbit = None
-        #self.conexaoRabbit()
-        #self.protocolos = {"http":0, "ssdp":0, "ssl":0, "dhcp":0, "ssh":0, "unknown":0, "all":0}
-        
-        self.status = "0"
-        
-    def conexaoRabbit(self, ipcoletor):
-        self.credentials = pika.PlainCredentials('skywalker', 'luke') #criar user no CONSUMIDOR (receive) e permissoes no vhost
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(str(ipcoletor), 5672, '/starwars', self.credentials)) #criar o vhost
-        self.channel = self.connection.channel()
-        
-        self.result = self.channel.queue_declare(exclusive = True)
-        self.queue_name = self.result.method.queue
-        
-        self.binding_keys = ["http", "ssdp", "ssl", "dhcp", "ssh", "unknown", "all"]
-        
-        #CHECAR O FOR QUE TA DANDO ERRO!
-        for binding_key in self.binding_keys:
-            
-            self.result = self.channel.queue_declare(exclusive = True)
-            self.queue_name = self.result.method.queue
-            self.channel.queue_bind(exchange = "topic_logs", queue = self.queue_name, routing_key = binding_key)
-    
+
     def iniciarMonitor(self):
         self.broadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.broadcastSocket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.broadcastSocket.bind((self.hostBroadcast, self.portaRecebeBroadcast))
         
-    def getTamanhoPacote(self):
-        return self.tamanhoPacote
+        while True:
+            try:
+                
+                message, address = self.broadcastSocket.recvfrom(2048)
+                
+                if message == "MONITOR":
+                    if address[0] not in  self.coletoresConectados.keys():
+                        self.coletoresConectados[(address[0])] = "Coletando"
+                    self.broadcastSocket.sendto("APRESENTAR", address)
+            
+            except (KeyboardInterrupt, SystemExit):
+                self.broadcastSocket.close()
+                
+    def receberComando(self):
+        comandos = {"LISTAR": "Lista coletores conectados", "CONTINUAR": "Continua a coleta em um coletor parado", "PARAR": "Para a coleta de um coletor"}
+        print("Lista de comandos:")
+        for nome, desc in comandos.iteritems():
+            print("-" + nome + ": " + desc)
+        print("\n")
+        
+        while True:
+            escolha = ""
+            while escolha not in comandos.keys():
+                escolha = raw_input("Escolha um comando da lista de comandos: ")
+
+            if escolha !="LISTAR":
+                print"LISTAR"
+            else:
+                self.listarColetores()
+        
+        
+    def listarColetores(self):
+        print("Coletores conectados: ")
+        for coletor, status in self.getColetoresConectados().iteritems():
+            print("-" + str(coletor) + ": " + status)
+        print("\n")
     
     def getColetoresConectados(self):
         return self.coletoresConectados
-            
-    def getColetorAtual(self):
-        return self.coletorAtual
-        
-    def setColetorAtual(self, coletorAtual):
-        self.coletorAtual = coletorAtual
         
     def getBroadcastSocket(self):
         return self.broadcastSocket
@@ -63,96 +65,7 @@ class Monitor:
     def getPortaEnvioBroadcast(self):
         return self.portaEnvioBroadcast
         
-    def abrirConexoes(self):
-        while True:
-            try:
-                print ("R2D2: Aguardando...")
-                mensagemColetor, enderecoColetor = self.broadcastSocket.recvfrom(self.getTamanhoPacote())
-                if mensagemColetor == "DESCOBRIR":
-                    print("R2D2: Descoberto por coletor ") + str(enderecoColetor)
-                    ip, porta = enderecoColetor
-                    if not self.coletoresConectados.has_key(ip):
-                        self.coletoresConectados[ip] = enderecoColetor
-                        print "R2D2: " +ip+" adicionado à lista de coletores."
-                    mensagemMonitor = 'DESCOBERTO'
-                    self.broadcastSocket.sendto(mensagemMonitor, enderecoColetor)
-                return False
-            
-            except (KeyboardInterrupt, SystemExit):
-                os.system('clear')
-                break
-    
-    
-        
-        
 if __name__ == '__main__':
     r2d2 = Monitor()
-    broadcastSocket = r2d2.getBroadcastSocket()
-    
-    print 'R2D2: Monitor ligado.'
-    
-    while True:
-        print 'R2D2: Menu:'
-        print '\n'
-        print '[1] REVELAR-SE'
-        print '[2] LISTAR COLETORES'
-        print '[0] SAIR'
-        print '\n'
-        keyboardInput = raw_input('>> ')
-        
-        if keyboardInput == "1":
-            r2d2.abrirConexoes()
-            os.system("clear")
-            mensagemMonitor, enderecoColetor = broadcastSocket.recvfrom(r2d2.getTamanhoPacote())
-            print mensagemMonitor
-            continue
-            
-        elif keyboardInput == "2":
-            os.system("clear")
-            coletores = r2d2.getColetoresConectados()
-            print 'R2D2: Listando coletores:\n'
-            if coletores == {}:
-                print "R2D2: Nenhum coletor conectado.\n"
-                continue
-            else:
-                keys = coletores.keys()
-                listaColetores={}
-                for n, coletor in enumerate(keys):
-                    print "(%d) - coletor %s \n" % (n+1, coletor)
-                    listaColetores[n+1] = coletor
-                print("R2D2: Selecione o coletor:\n")
-                keyboardInput = input(">> ")
-                os.system("clear")
-                if keyboardInput in listaColetores:
-                    enderecoColetor = listaColetores[keyboardInput]
-                    print '[1] CAPTURAR'
-                    print '[2] SUSPENDER'
-                    print '[0] SAIR'
-                    keyboardInput = raw_input(">> ")
-                    if keyboardInput == "1":
-                        r2d2.broadcastSocket.sendto("CAPTURAR", (enderecoColetor, r2d2.getPortaEnvioBroadcast()))
-                        ipcoletor, porta = enderecoColetor
-                        r2d2.conexaoRabbit(ipcoletor)
-                        #print "Foi"
-                    elif keyboardInput == "2":
-                        r2d2.broadcastSocket.sendto("SUSPENDER", (enderecoColetor, r2d2.getPortaEnvioBroadcast()))
-                    elif keyboardInput == "0":
-                        os.system("clear")
-                        break
-                    else:
-                        print("R2D2: Opção inválida")
-                        continue
-                else:
-                    print("R2D2: Coletor inválido")
-        
-        elif keyboardInput == "0":
-            os.system("clear")
-            break
-        else:
-            print("R2D2: Opção inválida")
-            continue
-        
-                
-        
-    broadcastSocket.close()
-    print "R2D2 encerrado."
+    r2d2.iniciarMonitor()
+    r2d2.receberComando()
