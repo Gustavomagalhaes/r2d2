c189d25583dddcf394ff4be1c69957fbf6411304
Robson Ugo Souza <ulrichugo@gmail.com>
2015-10-14 13:33:57 +0000

    timeout monitor

--- projeto/monitor.py
+++ projeto/monitor.py
@@ -48,8 +48,7 @@ class Monitor:
             if endereco[0] not in coletores.keys():
                 self.setColetor(endereco[0])
                 print "R2D2: Coletor adicionado a lista de coletores."
-            #self.getClientSocket().close()
-            self.clientSocket.settimeout(0)
+            self.getClientSocket().close()
             continue
         
     def listarColetores(self):
