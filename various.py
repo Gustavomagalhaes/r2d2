f8c2a208f37064dc6aeb9b0dda2ce281d18c5479
Robson Ugo Souza <ulrichugo@gmail.com>
2015-11-29 13:54:21 +0000

    URGH

--- various.py
+++ various.py
@@ -61,7 +61,7 @@ class Various():
                     if chave == p[0]:
                         print chave + "\t" + str(p[0])
                         print "ARQUIVO:\t" + str(re.compile(valor))
-                        print "DICIONARIO:\t" + p[1]
+                        print "DICIONARIO:\t" + str(p[1])
                 protocolos[chave] = valor
             #protocolos = {"ssl":"^(.?.?\x16\x03.*\x16\x03|.?.?\x01\x03\x01?.*\x0b)", "ssh":"^ssh-[12]\.[0-9]", "ssdp":"^notify[\x09-\x0d ]\*[\x09-\x0d ]http/1\.1[\x09-\x0d -~]*ssdp:(alive|byebye)|^m-search[\x09-\x0d ]\*[\x09-\x0d ]http/1\.1[\x09-\x0d -~]*ssdp:discover", "bittorrent":"^(\x13bittorrent protocol|azver\x01$|get /scrape\?info_hash=)", "dhcp":"^[\x01\x02][\x01- ]\x06.*c\x82sc", "http":"[\x09-\x0d -~]*"}
                 
