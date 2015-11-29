b3b7f22a4b9e5bb95b46dd147c49c466b077c4cb
Robson Ugo Souza <ulrichugo@gmail.com>
2015-11-29 18:14:19 +0000

    mizinga

--- various.py
+++ various.py
@@ -45,32 +45,23 @@ class Various():
                         if linha[0] != "^" and linha[:5] != "http/" and linha[0] != "\n":
                             chave = str(linha.replace("\n",""))
                         elif  (linha[0] == "^" or linha[:5] == "http/") and linha[0] != "\n":
-                            valor = linha[1]
-                            valor = str(valor.replace("\n",""))
-                            #print valor[:6]
-                            if "?info_hash=get" in valor:
-                                valor = valor[:-67:]+")"
-                            elif "http/(0\.9|1\.0|1\.1)" in valor:
-                                valor = valor.split("(connection")
-                                valor = valor[0]
-                                valor = valor[-15::]
                             valor = (str(linha).lstrip()).rstrip()
                     linha = file.readline()
                                 
                 #protocolos[chave] = re.compile(valor)
                 #valor = re.compile(valor)
-                # proto = {}
-                # arq = open("protocolos.txt","r")
-                # listaLinhas = arq.readlines()
-                # for linha in listaLinhas:
-                #     linha = linha.split(":")
-                #     expr = linha[1].replace("\n","")
-                #     nome= linha[0]
-                #     proto[nome] = expr
+                proto = {}
+                arq = open("protocolos.txt","r")
+                listaLinhas = arq.readlines()
+                for linha in listaLinhas:
+                    linha = linha.split(":")
+                    expr = linha[1].replace("\n","")
+                    nome= linha[0]
+                    proto[nome] = expr
                 protocolos[chave] = valor
             #protocolos = {"ssl":"^(.?.?\x16\x03.*\x16\x03|.?.?\x01\x03\x01?.*\x0b)", "ssh":"^ssh-[12]\.[0-9]", "ssdp":"^notify[\x09-\x0d ]\*[\x09-\x0d ]http/1\.1[\x09-\x0d -~]*ssdp:(alive|byebye)|^m-search[\x09-\x0d ]\*[\x09-\x0d ]http/1\.1[\x09-\x0d -~]*ssdp:discover", "bittorrent":"^(\x13bittorrent protocol|azver\x01$|get /scrape\?info_hash=)", "dhcp":"^[\x01\x02][\x01- ]\x06.*c\x82sc", "http":"[\x09-\x0d -~]*"}
                 
-            return protocolos
+            return proto
             
     def classificarProtocolo(self, protocolo):
         for nome, p in self.listarProtocolos().iteritems():
@@ -102,7 +93,7 @@ class Various():
             
             ip = eth.data
             if isinstance(ip,dpkt.ip.IP):
-                mensagem = str(contPkt) + "-" + "Rede: IP.Tamanho: "+str(len(pkt))+".Timestamp: "+str(ts)
+                mensagem = "##IP#"+str(len(pkt))+"#"+str(ts)
                 print mensagem
                 #self.emit_topic("ip",mensagem)
                 #self.emit_topic("all",mensagem)
@@ -114,7 +105,7 @@ class Various():
                     elif isinstance(transp,dpkt.udp.UDP):
                         transporte = "UDP"
                     
-                    mensagem = str(contPkt) + "-" + "Transporte: "+transporte+".Rede: IP.Tamanho: "+str(len(pkt))+".Timestamp: "+str(ts)
+                    mensagem = "#"+transporte+"#IP#"+str(len(pkt))+"#"+str(ts)
                     print mensagem
                     #self.emit_topic(transporte,mensagem)
                     #self.emit_topic("all",mensagem)
@@ -125,15 +116,15 @@ class Various():
                     for p in protocolos.items():
                         expressao = re.compile(p[1])
                         if expressao.search(app):
-                            mensagem = "App: "+p[0]+".Transporte: "+transporte+".Rede: IP.Tamanho: "+str(len(pkt))+".Timestamp: "+str(ts)
-                            print str(contPkt) + "-" + mensagem
+                            mensagem = +p[0]+"#"+transporte+"#IP#"+str(len(pkt))+"#"+str(ts)
+                            print mensagem
                             #self.emit_topic(p[0],mensagem)
                             #self.emit_topic("all",mensagem)
                             self.contProtocolos[p[0]] += 1
                             found = True
         					
                         if (not found):
-                            mensagem = str(contPkt) + "-" + "App: "+p[0]+".Transporte: "+transporte+".Rede: IP.Tamanho: "+str(len(pkt))+".Timestamp: "+str(ts)
+                            mensagem = "UNKOWN#"+transporte+"#IP#"+str(len(pkt))+"#"+str(ts)
                             print mensagem
                             #self.emit_topic("unknown",mensagem)
                             self.contProtocolos["unknown"] += 1
