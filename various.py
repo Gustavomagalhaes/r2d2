56a91f380a408beca6a9646e739e70d7373af996
Robson Ugo Souza <ulrichugo@gmail.com>
2015-11-29 14:35:28 +0000

    valor

--- various.py
+++ various.py
@@ -65,7 +65,7 @@ class Various():
                         print str(valor[0:7])
                         if valor[0:7] == "^notify":
                             valor.split(":(alive")
-                            valor = valor[0]
+                            print str(valor)
                         # elif valor[5:16] == "bittorrent":
                         #     valor.split("=get")
                         #     valor = valor[0] + ")"
