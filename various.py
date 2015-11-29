50f9328871bde974f7493d672eef1602baee7321
Robson Ugo Souza <ulrichugo@gmail.com>
2015-11-29 12:35:09 +0000

    NOT FOUND

--- various.py
+++ various.py
@@ -75,17 +75,7 @@ class Various():
         connection.close()
     
     def iniciarColeta(self, file="", tempo = 60):
-        #assinaturas de protocolos de camada de aplicacao
-        expr="\x01\x10\x00\x01"
-        nbns = re.compile(expr)
-        expr="^\x7b\x22\x68\x6f"
-        dropbox = re.compile(expr)
-        expr="^[\x01\x02][\x01- ]\x06.*c\x82sc"
-        dhcp = re.compile(expr)
-        
         protocols = self.listarProtocolos()
-        
-        #contadores
         cnt = self.getcontProtocolos()
         cNonIP = 0
         
