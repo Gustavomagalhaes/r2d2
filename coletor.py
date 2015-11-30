2101a78aaf2171d32db403a3eadde95c773eaf53
Gustavo Magalhaes <itsgustavopereira@gmail.com>
2015-11-30 03:18:41 +0000

    afff

--- coletor.py
+++ coletor.py
@@ -227,7 +227,9 @@ class Coletor():
         channel.exchange_declare(exchange='topic_logs',type='topic')
         channel.basic_publish(exchange='topic_logs',routing_key=routing_key,body=mensagem)
         
-        print " [Enviado para fila %s] %r" % (routing_key.upper(), mensagem)
+        msg = mensagem.split('#')
+        
+        print "Enviado para fila [%s] %r" % (routing_key.upper(), msg)
         
         connection.close()
 
