e2e1e98a2743279deb3ca6f169698b3f2d9ac21c
Robson Ugo Souza <ulrichugo@gmail.com>
2015-12-08 22:19:57 +0000

    PAAAHI

--- testSocketError-coletor.py
+++ testSocketError-coletor.py
@@ -56,7 +56,7 @@ while True:
                             mensagem, endereco = downloadSocket.recvWithError(8192)
                     except:
                         traceback.print_exc()
-                        print "Não conseguiu enviar no ACK-NACK - Timeout"
+                        print "Nao conseguiu enviar no ACK-NACK - Timeout"
         except:
             traceback.print_exc()
             print "Nem entrou no try do add buffers - caiu no except"
