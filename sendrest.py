afbd79beb190a8f78ca27e934e618e6aac5ec057
blendaguedes <blendaguedes@hotmail.com>
2015-12-09 03:33:18 +0000

    classe que fara a conexao com o rest :D

---
+++ sendrest.py
@@ -0,0 +1,44 @@
+import pika
+import sys
+import os
+from threading import Thread
+
+class SendRest(Thread):
+        
+    def __init__(self):
+        
+        Thread.__init__(self)
+        self.__credentials = pika.PlainCredentials('skywalker', 'luke')
+        self.__connection = pika.BlockingConnection(pika.ConnectionParameters(
+                       '192.168.25.61', 5672, '/starwars', self.__credentials))
+        self.__channel = self.__connection.channel()
+        self.__porcentagem = {"elefante":0, "rato":0, "tartaruga":0, "libelula":0, "caramujo":0, "guepardo":0} 
+            
+    def getPorcentagem(self):
+        return self.__porcentagem
+        
+        
+    def run(self):
+        
+        
+        self.__channel.exchange_declare(exchange='topic_logs',
+                                     type='topic')
+        result = self.__channel.queue_declare(exclusive=True)
+        queue_name = result.method.queue
+        self.__channel.basic_consume(self.callback, queue=queue_name, no_ack=True)
+        queue_name = result.method.queue
+
+    
+        
+    def callback(self, ch, method, properties, body):
+        #print " [x] %r:%r" % (method.routing_key, body,)
+        animais = body.split("|")
+        
+        for animal in animais:
+            self.__porcentagem[animal] +=1
+        
+     
+      
+       
+    
+ 
